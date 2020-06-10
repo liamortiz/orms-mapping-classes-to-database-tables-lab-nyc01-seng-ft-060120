2d6fb9dcb997ac152dc88b7bf1c46bd9eb91d8ce
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-10 21:55:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
     QUERY
     DB[:conn].execute(query, self.name, self.grade)
     
-    last_id = DB[:conn].execute("SELECT id FROM students")
+    last_id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0]
     p last_id
   end
   
